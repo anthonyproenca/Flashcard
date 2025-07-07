fd60c3d513e1cabcb66256089806fb3fed896b7b
anthonyproenca <anthony.proenca@escola.pr.gov.br>
2025-07-07 11:42:04 -0300

    qaz

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,15 @@
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+   super herois dc,
+    ,qual personagem usa uma capa em formato de morcego 
+    batman 
+)
+criaCartao(
+    super heroi marvel ,
+    qual personagem usa um martelo,
+    thor
+)
+criaCartao(
+   futebol ,
+    quantas copas do mundo o brasil ganhou,
+    5 vezes
 )
