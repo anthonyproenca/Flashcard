94d63fb97d3fe68715c4aa12e890e259fd4ea604
anthonyproenca <anthony.proenca@escola.pr.gov.br>
2025-08-11 08:59:15 -0300

    jbkjbjb

--- perguntas.js
+++ perguntas.js
@@ -1,15 +1,30 @@
 criaCartao(
-   super herois dc,
-    ,qual personagem usa uma capa em formato de morcego 
-    batman 
+   'super herois dc',
+   ' qual personagem usa uma capa em formato de morcego', 
+   ' batman' 
 )
 criaCartao(
-    super heroi marvel ,
-    qual personagem usa um martelo,
-    thor
+    'super heroi marvel' ,
+    qual personagem usa um martelo',
+    'thor'
 )
 criaCartao(
-   futebol ,
-    quantas copas do mundo o brasil ganhou,
-    5 vezes
+'futebol', 
+    'quantas copas do mundo o brasil ganhou',
+    '5 vezes'
+)
+criaCartao(
+'futebol', 
+    'qual o ultimo vencedor da copa do mundo',
+    'Argentina'
+)
+criaCartao(
+'futebol', 
+    'qual o ultimo campeão da copa do mundo de clubes',
+    'chelsea'
+)
+criaCartao(
+'futebol', 
+    'qual o ultimo campeão do paulistão ',
+    'corinthians'
 )
